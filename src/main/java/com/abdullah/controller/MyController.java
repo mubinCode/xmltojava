e83f269022353adbab372585e3b47684221ee9a9
mubinCode <amubineee@gmail.com>
2023-11-19 00:13:00 +0600

    "updated MyController and SpaceStation class"

--- src/main/java/com/abdullah/controller/MyController.java
+++ src/main/java/com/abdullah/controller/MyController.java
@@ -10,15 +10,18 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 
 @RestController
 public class MyController {
 
     @GetMapping(value = "/space", produces = {"application/json"})
     SpaceStation spaceStation() throws XMLStreamException, IOException {
-        InputStream xmlResource = MyController.class.getClassLoader().getResourceAsStream("station.xml");
+        String xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><spacestation><owner country=\"USA\" leader=\"trump\"/><count>3</count><spaceships><spaceship><name>Falcon</name><numberOfCrew>10</numberOfCrew><price currency=\"US\" validity=\"oneMonth\">1500.0</price></spaceship><spaceship><name>Strom</name><numberOfCrew>25</numberOfCrew><price currency=\"Euro\">1500.0</price></spaceship><spaceship><name>Serum</name><numberOfCrew>9</numberOfCrew><price currency=\"DKK\">1500.0</price></spaceship></spaceships></spacestation>";
+//        InputStream xmlResource = MyController.class.getClassLoader().getResourceAsStream("station.xml");
         XMLInputFactory factory = XMLInputFactory.newFactory();
-        XMLStreamReader reader = factory.createXMLStreamReader(xmlResource);
+//        XMLStreamReader reader = factory.createXMLStreamReader(xmlResource);
+        XMLStreamReader reader = factory.createXMLStreamReader(new StringReader(xmlString));
 
         XmlMapper mapper = new XmlMapper();
         SpaceStation spaceStation = mapper.readValue(reader, SpaceStation.class);
--- src/main/java/com/abdullah/spacestation/SpaceStation.java
+++ src/main/java/com/abdullah/spacestation/SpaceStation.java
@@ -1,10 +1,14 @@
 package com.abdullah.spacestation;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class SpaceStation {
     private int count;
     private Owner owner;
